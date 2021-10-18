8751a058686ab7662d2d02606b74112b12a48019
arnonrdp <arnonrdp@gmail.com>
2021-10-18 10:18:00 -0300

    Serialização

---
+++ src/devdojo/maratonajava/javacore/x/serializacao/test/SerializacaoTest.java
@@ -0,0 +1,37 @@
+package devdojo.maratonajava.javacore.x.serializacao.test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import devdojo.maratonajava.javacore.x.serializacao.domain.Aluno;
+
+public class SerializacaoTest {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "Kuririn", "123456");
+
+        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+            oos.writeObject(aluno);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar() {
+        Path path = Paths.get("aluno.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            Aluno aluno = (Aluno) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
