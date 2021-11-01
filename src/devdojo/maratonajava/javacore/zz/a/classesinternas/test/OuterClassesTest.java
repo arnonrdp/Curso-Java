ee9dcc3407b6f6cf6e8e2c8bfad13678a6abc837
arnonrdp <arnonrdp@gmail.com>
2021-11-01 10:34:00 -0300

    Classes Internas: Outer Classes

---
+++ src/devdojo/maratonajava/javacore/zz/a/classesinternas/test/OuterClassesTest.java
@@ -0,0 +1,41 @@
+package devdojo.maratonajava.javacore.zz.a.classesinternas.test;
+
+
+public class OuterClassesTest {
+    private String name = "Midoriya";
+
+    class Inner {
+        public void printOuterClassAtrribute() {
+            System.out.println(name);
+        }
+    }
+
+    void print() {
+        String lastName = "Izuku";
+        class LocalClass {
+            public void printLocal() {
+                System.out.println(name + " " + lastName);
+            }
+        }
+        new LocalClass().printLocal();
+    }
+
+    static class Nested {
+        private String lastName = "Izuku";
+        void print() {
+            System.out.println(new OuterClassesTest().name + " " + lastName);
+        }
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest outerClasses1 = new OuterClassesTest();
+        Inner inner = outerClasses1.new Inner();
+        inner.printOuterClassAtrribute();
+
+        OuterClassesTest outerClasses2 = new OuterClassesTest();
+        outerClasses2.print();
+
+        new OuterClassesTest.Nested().print();
+    }
+
+}
