5341d1b2ceb0213e45e19504743edbd75772e0f9
arnonrdp <arnonrdp@gmail.com>
2021-11-06 14:50:00 -0300

    Optional

---
+++ src/devdojo/maratonajava/javacore/zd/optional/test/OptionalTest.java
@@ -0,0 +1,44 @@
+package devdojo.maratonajava.javacore.zd.optional.test;
+
+import java.util.List;
+import java.util.Optional;
+
+import devdojo.maratonajava.javacore.zd.domain.Manga;
+import devdojo.maratonajava.javacore.zd.repository.MangaRepository;
+
+public class OptionalTest {
+    public static void main(String[] args) {
+        Optional<String> o1 = Optional.of("Aha uhu o DevDojo é foda.");
+        Optional<String> o2 = Optional.ofNullable(null);
+        Optional<String> o3 = Optional.empty();
+
+        System.out.println(o1);
+        System.out.println(o2);
+        System.out.println(o3);
+        System.out.println("-------");
+
+        Optional<String> nameOptional = findName("William");
+        String empty = nameOptional.orElse("EMPTY");
+        System.out.println(empty);
+        nameOptional.ifPresent(s -> System.out.println(s.toUpperCase()));
+
+        Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Boku no Hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2).orElseThrow(IllegalArgumentException::new);
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.findByTitle("Drifters").orElseGet(() -> new Manga(3, "Drifters", 20));
+        System.out.println(newManga);
+    }
+
+    private static Optional<String> findName(String name) {
+        List<String> list = List.of("William", "DevDojo");
+        int i = list.indexOf(name);
+        if (i != 1) {
+            return Optional.of(list.get(i));
+        }
+        return Optional.empty();
+    }
+}
